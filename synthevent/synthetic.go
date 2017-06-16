7f26991b8bd6f71fb752f8875da1e53b35a15228
d-smith <doug.smith.mail@gmail.com>
2017-06-16 11:40:30 -0700

    Added conversion to publishable event type

--- synthevent/synthetic.go
+++ synthevent/synthetic.go
@@ -1,24 +1,46 @@
 package synthevent
 
 import (
+	"github.com/golang/protobuf/proto"
+	"github.com/xtracdev/goes"
 	"github.com/xtracdev/goes/uuid"
+	"math/rand"
 	"time"
 )
 
-func NewSyntheticEvent() (*SyntheticEvent,error) {
-	id,err := uuid.GenerateUuidV4()
+const (
+	EventTypeCode = "SYNTH"
+)
+
+var r = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func NewSyntheticEvent() (*SyntheticEvent, error) {
+	id, err := uuid.GenerateUuidV4()
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	now := time.Now()
 	return &SyntheticEvent{
-		EventId:id,
-		Injected:now.UnixNano(),
-	},nil
+		EventId:  id,
+		Injected: now.UnixNano(),
+	}, nil
 }
 
 func (e *SyntheticEvent) GetInjectedTime() time.Time {
 	return time.Unix(0, e.Injected)
 }
 
+func (e *SyntheticEvent) ToGoESEvent() (*goes.Event, error) {
+	var newEvent goes.Event
+	var err error
+	newEvent.Source = e.EventId
+	newEvent.Version = r.Int()
+	newEvent.TypeCode = EventTypeCode
+	newEvent.Payload, err = proto.Marshal(e)
+	if err != nil {
+		return nil, err
+	}
+
+	return &newEvent, nil
+}
--- synthevent/synthevent_test.go
+++ synthevent/synthevent_test.go
@@ -1,19 +1,37 @@
 package synthevent
 
 import (
-	"testing"
-	"github.com/stretchr/testify/assert"
 	"github.com/golang/protobuf/proto"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
 func TestMarshalling(t *testing.T) {
 
 	synthEvent, err := NewSyntheticEvent()
-	if assert.Nil(t,err) {
-		marshalled,err := proto.Marshal(synthEvent)
+	if assert.Nil(t, err) {
+		marshalled, err := proto.Marshal(synthEvent)
 		if assert.Nil(t, err) {
 			var unpickled SyntheticEvent
 			err := proto.Unmarshal(marshalled, &unpickled)
+			if assert.Nil(t, err) {
+				assert.Equal(t, synthEvent.EventId, unpickled.EventId)
+				assert.True(t, synthEvent.GetInjectedTime().Equal(unpickled.GetInjectedTime()))
+			}
+		}
+	}
+}
+
+func TestEventConversion(t *testing.T) {
+	synthEvent, err := NewSyntheticEvent()
+	if assert.Nil(t, err) {
+		e, err := synthEvent.ToGoESEvent()
+		if assert.Nil(t, err) {
+			assert.Equal(t, synthEvent.EventId, e.Source)
+			assert.Equal(t, EventTypeCode, e.TypeCode)
+
+			var unpickled SyntheticEvent
+			err = proto.Unmarshal(e.Payload.([]byte), &unpickled)
 			if assert.Nil(t, err) {
 				assert.Equal(t, synthEvent.EventId, unpickled.EventId)
 				assert.True(t, synthEvent.GetInjectedTime().Equal(unpickled.GetInjectedTime()))
